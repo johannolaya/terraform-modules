2709d890ac285b4c5000c191486dea9496d5e2b7
root <root@pop-os.localdomain>
2019-02-18 16:57:00 -0500

    add files web app service module

--- web-app-basic/main.tf
+++ web-app-basic/main.tf
@@ -7,11 +7,6 @@ locals {
   resourcegroup = "${var.location_g}-app-${var.name_prefix_g}-${var.channel_g}"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "${var.location_g}-${var.name_prefix_g}-rg-${var.channel_g}"
-  location = "${var.location_g}"
-}
-
 terraform {
   backend "azurerm" {}
 }
