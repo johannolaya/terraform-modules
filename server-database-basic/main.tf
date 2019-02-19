cd53dcac453492df69ea7805f5a967770c33dc46
root <root@pop-os.localdomain>
2019-02-19 11:54:15 -0500

    add parameters modules terraform

--- server-database-basic/main.tf
+++ server-database-basic/main.tf
@@ -1,16 +1,4 @@
-  resource "azurerm_sql_server" "sdb" {
-    name = "${var.location}-sdb-${var.name_prefix_g}-${var.channel_g}"
-    resource_group_name = "${var.rg_name}"
-    location = "${var.location}"
-    version = "12.0"
-    administrator_login = "${var.admin_username_db}"
-    administrator_login_password = "${var.admin_password_db}"
-    tags {
-      environment = "${var.name_prefix_g}"
-    }
-  }
-
-  resource "azurerm_sql_server" "sdb" {
+ resource "azurerm_sql_server" "sdb" {
     name = "${var.location}-sdb-${var.name_prefix_g}-${var.channel_g}"
     resource_group_name = "${var.rg_name}"
     location = "${var.location}"
