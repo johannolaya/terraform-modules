37f0861c1b373c7d900d9c17c9d20e7136110a18
root <root@pop-os.localdomain>
2019-02-20 09:46:58 -0500

    update modules web app and server database

--- server-database-basic/outputs.tf
+++ server-database-basic/outputs.tf
@@ -16,10 +16,18 @@ output "db_password" {
     "azurerm_sql_database.db"]
 }
 
-output "db_connection_base" {
+output "db_connection_app_base" {
   value = "Server=tcp:${azurerm_sql_server.sdb.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.sdb.administrator_login};Password=${azurerm_sql_server.sdb.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
-output "db_connection_slot" {
+output "db_connection_app_slot" {
   value = "Server=tcp:${azurerm_sql_server.sdb.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db_slot.name};Persist Security Info=False;User ID=${azurerm_sql_server.sdb.administrator_login};Password=${azurerm_sql_server.sdb.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
+
+output "db_connection_base" {
+value = "jdbc:sqlserver://${azurerm_sql_server.sdb.fully_qualified_domain_name}:1433;database=${azurerm_sql_database.db.name};"
+}
+
+output "db_connection_slot" {
+value = "jdbc:sqlserver://${azurerm_sql_server.sdb.fully_qualified_domain_name}:1433;database=${azurerm_sql_database.db_slot.name};"
 }
