e8149aa0f2223ce1507115d55105d70e3e8bea3a
JohannaB <johanna.buitrago@bizagi.com>
2019-02-25 12:28:43 -0500

    update modules

--- dns-binding/main.tf
+++ dns-binding/main.tf
@@ -13,13 +13,16 @@ resource "aws_route53_record" "aws-route" {
     "${var.app_url}.azurewebsites.net"]
 }
 
-resource "azurerm_app_service_custom_hostname_binding" "binding_app" {
-  hostname = "${var.app_dns}.${var.aws_domain_bizagi}"
-  app_service_name = "${var.app_name}"
-  resource_group_name = "${var.rg_name}"
-  depends_on = [
-    "aws_route53_record.aws-route"]
+resource "aws_route53_record" "aws-route-slot" {
+  zone_id = "${data.aws_route53_zone.domain.zone_id}"
+  name = "${var.app_dns}slot.${data.aws_route53_zone.domain.name}"
+  type = "${var.aws_type}"
+  ttl = "${var.aws_ttl}"
+  records = [
+    "${var.app_url}-slot.azurewebsites.net"]
+
 }
+
 
 data "template_file" "ssl_binding" {
   template = "${file("ssl_binding.json")}"
--- webapp-basic/outputs.tf
+++ webapp-basic/outputs.tf
@@ -9,3 +9,7 @@ output "app_name" {
 output "app_slot_name" {
   value = "${azurerm_app_service_slot.app_slot.name}"
 }
+
+output "sp_id" {
+  value = "${azurerm_app_service_plan.sp.id}"
+}
