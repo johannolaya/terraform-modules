db7603abb24e161000c34d03c69a42527670c6a5
felipe rojas <feliperojas@pop-os.localdomain>
2019-02-27 21:05:22 -0500

    add name and key to storage account outputs

--- storage-account-basic/outputs.tf
+++ storage-account-basic/outputs.tf
@@ -1,3 +1,11 @@
 output "sa_connection" {
   value = "${azurerm_storage_account.sa.primary_connection_string}"
 }
+
+output "sa_name" {
+  value = "${azurerm_storage_account.sa.name}"
+}
+
+output "sa_key" {
+  value = "${azurerm_storage_account.sa.primary_access_key}"
+}
