ac4d42a5b27b6c8fcdc35f0c15558b7846844503
Johann Olaya <Johann.Olaya@bizagi.com>
2019-03-05 14:40:58 -0500

    add webapp id in output file

--- webapp-basic/outputs.tf
+++ webapp-basic/outputs.tf
@@ -6,8 +6,16 @@ output "app_name" {
   value = "${azurerm_app_service.app.name}"
 }
 
+output "app_id" {
+  value = "${azurerm_app_service.app.id}"
+}
+
 output "app_slot_name" {
   value = "${azurerm_app_service_slot.app_slot.name}"
+}
+
+output "app_slot_id" {
+  value = "${azurerm_app_service.app.id}"
 }
 
 output "sp_id" {
