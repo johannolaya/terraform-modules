3c50d21bb6c066c3696f36547147ce7311858317
Johann Olaya <Johann.Olaya@bizagi.com>
2019-03-14 10:01:55 -0500

    add app insights name

--- webapp-basic/main.tf
+++ webapp-basic/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_app_service" "app" {
   app_settings = "${merge(
     map("APPINSIGHTS_INSTRUMENTATIONKEY",local.app_instrumentation_key),
     map(var.app_insights_name ,local.app_instrumentation_key),
-    map("${null_resource.app_insights_setting.*.triggers.setting_name}","${null_resource.app_insights_setting.*.triggers.value}"),
+    "${null_resource.app_insights_setting.*.triggers}",
     var.app_settings)}"
 
   connection_string {
